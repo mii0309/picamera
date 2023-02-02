2bb419cbdcd08636d2cc5fcbdc04c149b3197c37
mii0309 <101243844+mii0309@users.noreply.github.com>
2023-02-02 22:25:52 +0900

    Create catschase.py

---
+++ catschase.py
@@ -0,0 +1,28 @@
+import cv2 
+import sys 
+import picamera.array
+import picamera
+
+cascade_file="cascade.xml"
+with picamera.PiCamera() as camera:
+  with picamera.array.PiRGBAarray(camera) as stream:
+    camera.resolution=(320,240)
+    While True:
+      camera.capture(stream,'bgr',use_video_port=True)
+      cascade=cv2.CascadeClassifier(cascadefilename)
+     if cascade.empty():
+        print('cannot load cascade file')
+        sys.exit(-1)
+     objects=cascade.detectMultiScale(srcimg,1.1,3)
+    
+     for(x,y,w,h) in objects:
+        print(x,y,w,h) 
+        cv2.rectangle(dstimg,(x,y),(x+w,y+h),(0,0,255),5)
+        cv2.imshow('frame',stream.array)
+        
+        if cv2.waitkey(1)&0xFF=ord("q"):
+          break
+          
+        stream.seek(0)
+        stream.truncate()
+        cv2.destroyALLWindows()
