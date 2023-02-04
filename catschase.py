cf06e1a3e8031bd4ee69aeabe86115589a7f4f56
mii0309 <101243844+mii0309@users.noreply.github.com>
2023-02-04 11:56:24 +0900

    Update catschase.py

--- catschase.py
+++ catschase.py
@@ -6,7 +6,7 @@ import numpy as np
 cascade_file="cascade.xml"
 with picamera.PiCamera() as camera:
   with picamera.array.PiRGBArray(camera) as stream:
-    camera.resolution=(320,240)
+    camera.resolution=(640,480)
     while True:
       camera.capture(stream,'bgr',use_video_port=True)
       gray = cv2.cvtColor(stream.array, cv2.COLOR_BGR2GRAY)
@@ -14,7 +14,9 @@ with picamera.PiCamera() as camera:
       objects=cascade.detectMultiScale(gray, minSize=(100,100))
       for(x,y,w,h) in objects:
         print(x,y,w,h) 
-        cv2.rectangle(stream.array, (x, y), (x+w, y+h), (0,0,255),5)
+        color = (0, 0, 255)
+        pen_w = 5
+        cv2.rectangle(stream.array, (x, y), (x+w, y+h), color, thickness = pen_w)
         cv2.imshow('frame',stream.array)
         
       if cv2.waitKey(1)&0xFF==ord("q"):
