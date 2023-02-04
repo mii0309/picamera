a07ee03f49c20263ebc7d91531941c939ed347c3
mii0309 <101243844+mii0309@users.noreply.github.com>
2023-02-04 12:10:00 +0900

    Update catschase.py

--- catschase.py
+++ catschase.py
@@ -21,6 +21,6 @@ with picamera.PiCamera() as camera:
         
       if cv2.waitKey(1)&0xFF==ord("q"):
         break
-    stream.seek(0)
-    stream.truncate(0)
+      stream.seek(0)
+      stream.truncate(0)
     cv2.destroyALLWindows()
